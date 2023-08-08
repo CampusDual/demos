dfe98a628a8ba9b8a8eac4e88d36d0d15ab81a76
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2023-08-08 11:30:25 +0200

    Updated script to use paths relative to the script execution directory

--- public/2023-BFS-1-G1_Partners/run_app.bat
+++ public/2023-BFS-1-G1_Partners/run_app.bat
@@ -1,32 +1,45 @@
 @echo off
+setlocal
+
+for %%F in ("%~dp0.") do set "initial_dir=%%~fF"
+
 echo ==== Detener app con docker compose (1/10) ====
 wsl.exe docker compose down
+
 echo ==== Crear carpeta temporal (2/10) ====
-mkdir temp
-cd temp
+mkdir "%initial_dir%\temp"
+cd "%initial_dir%\temp"
+
 echo ==== Clonar repositorio (3/10) ====
 git clone https://github.com/CampusDual/2023-BFS-1-G1_Partners
 cd 2023-BFS-1-G1_Partners
 git checkout docker
+
 echo ==== Limpiando imagenes antiguas (4/10) ====
 wsl.exe docker image rm 2023-bfs-1-g1-db
 wsl.exe docker image rm 2023-bfs-1-g1-backend
 wsl.exe docker image rm 2023-bfs-1-g1-frontend
+
 echo ==== Creando la imagen de la BBDD (5/10) ====
-cd ontimizeBackend\ontimizeBackend-model\src\main\db\
+cd "%initial_dir%\temp\2023-BFS-1-G1_Partners\ontimizeBackend\ontimizeBackend-model\src\main\db"
 wsl.exe docker build -t 2023-bfs-1-g1-db .
+
 echo ==== Creando la imagen del backend (6/10) ====
-cd ../../../../
+cd "%initial_dir%\temp\2023-BFS-1-G1_Partners\ontimizeBackend"
 wsl.exe docker build -t 2023-bfs-1-g1-backend .
-cd ..
+
 echo ==== Creando la imagen del frontend (7/10) ====
-cd frontEnd_ontimize/
+cd "%initial_dir%\temp\2023-BFS-1-G1_Partners\frontEnd_ontimize"
 wsl.exe docker build -t 2023-bfs-1-g1-frontend .
-cd ..
+
 echo ==== Eliminando repositorios (8/10) ====
-cd ../../
+cd %initial_dir%
 rmdir /s /q "temp"
+
 echo ==== Lanzando docker compose (9/10) ====
 wsl.exe docker compose up -d
+
 echo ==== Visualizando logs de docker compose (10/10) ====
 wsl.exe docker compose logs -f
+
+endlocal
--- public/2023-BFS-1-G1_Partners/run_app.sh
+++ public/2023-BFS-1-G1_Partners/run_app.sh
@@ -1,33 +1,42 @@
 #!/bin/bash
+
+initial_dir=$(cd "$(dirname "$0")" && pwd)
+
 echo "==== Detener app con docker compose (1/10) ===="
-wsl.exe docker compose down
+docker compose down
+
 echo "==== Crear carpeta temporal (2/10) ===="
-mkdir temp
-cd temp
+mkdir "$initial_dir/temp"
+cd "$initial_dir/temp"
+
 echo "==== Clonar repositorio (3/10) ===="
 git clone https://github.com/CampusDual/2023-BFS-1-G1_Partners
 cd 2023-BFS-1-G1_Partners
 git checkout docker
+
 echo "==== Limpiando imagenes antiguas (4/10) ===="
 docker image rm 2023-bfs-1-g1-db
 docker image rm 2023-bfs-1-g1-backend
 docker image rm 2023-bfs-1-g1-frontend
+
 echo "==== Creando la imagen de la BBDD (5/10) ===="
-cd ontimizeBackend/ontimizeBackend-model/src/main/db/
+cd "$initial_dir/temp/2023-BFS-1-G1_Partners/ontimizeBackend/ontimizeBackend-model/src/main/db"
 docker build -t 2023-bfs-1-g1-db .
+
 echo "==== Creando la imagen del backend (6/10) ===="
-cd ../../../../
+cd "$initial_dir/temp/2023-BFS-1-G1_Partners/ontimizeBackend"
 docker build -t 2023-bfs-1-g1-backend .
-cd ..
+
 echo "==== Creando la imagen del frontend (7/10) ===="
-cd frontEnd_ontimize/
+cd "$initial_dir/temp/2023-BFS-1-G1_Partners/frontEnd_ontimize"
 docker build -t 2023-bfs-1-g1-frontend .
-cd ..
+
 echo "==== Eliminando repositorios (8/10) ===="
-cd ../../
+cd "$initial_dir"
 rm -rf "temp"
+
 echo "==== Lanzando docker compose (9/10) ===="
 docker compose up -d
+
 echo "==== Visualizando logs de docker compose (10/10) ===="
 docker compose logs -f
-
