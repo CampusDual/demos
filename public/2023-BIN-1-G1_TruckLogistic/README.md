8b088714dcc024c32e145e12ad365db16628d51a
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2023-09-19 10:18:52 +0200

    Add 2023-BIN-1-G1_TruckLogistic demo

---
+++ public/2023-BIN-1-G1_TruckLogistic/README.md
@@ -0,0 +1,46 @@
+# 2023-BIN-1-G1_TruckLogistic
+## Repositorio
+* [Repositorio](https://github.com/CampusDual/2023-BIN-1-G1_TruckLogistic)
+## Presentación
+* [Visualización en Github](https://github.com/CampusDual/2023-BIN-1-G1_TruckLogistic/blob/main/TruckLogistic.pdf)
+* [Descargar PDF](https://raw.github.com/CampusDual/2023-BIN-1-G1_TruckLogistic/main/TruckLogistic.pdf)
+## Base de datos
+* Driver: `PostgreSQL`
+* Host: `localhost`
+* Puerto: `9055`
+* Base de datos: `2023-BIN-1-G1`
+* Usuario: `2023-BIN-1-G1`
+* Contraseña: `ZllztobVP8Xj67Oo38`
+## Backend y Frontend
+* Backend URL: [http://localhost:30072](http://localhost:30072)
+* Frontend URL: [http://localhost:4235](http://localhost:4235)
+* Usuario administrador: `demo`
+* Contraseña administrador: `demouser`
+* Usuario transportista: `pepe.rodriguez`
+* Contraseña transportista: `abc123`
+
+# Requisitos
+* Docker (con WSL)
+* Docker compose (v2)
+* Git
+
+# Windows
+## Iniciar aplicación
+```
+# 2023-BIN-1-G1_TruckLogistic> run_app.bat
+```
+## Parar la aplicación
+```
+# 2023-BIN-1-G1_TruckLogistic> wsl.exe docker compose down
+```
+
+# WSL
+## Iniciar aplicación
+```
+# 2023-BIN-1-G1_TruckLogistic> chmod +x run_app.sh
+# 2023-BIN-1-G1_TruckLogistic> ./run_app.sh
+```
+## Parar la aplicación
+```
+# 2023-BIN-1-G1_TruckLogistic> docker compose down
+```
---
+++ public/2023-BIN-1-G1_TruckLogistic/run_app.bat
@@ -0,0 +1,29 @@
+@echo off
+for %%F in ("%~dp0.") do set "initial_dir=%%~fF"
+echo ==== Detener app con docker compose (1/10) ====
+wsl.exe docker compose down
+echo ==== Crear carpeta temporal (2/10) ====
+mkdir "%initial_dir%\temp"
+cd "%initial_dir%\temp"
+echo ==== Clonar repositorio (3/10) ====
+git clone --single-branch --branch docker https://github.com/CampusDual/2023-BIN-1-G1_TruckLogistic
+echo ==== Limpiando imagenes antiguas (4/10) ====
+wsl.exe docker image rm 2023-bin-1-g1-db
+wsl.exe docker image rm 2023-bin-1-g1-backend
+wsl.exe docker image rm 2023-bin-1-g1-frontend
+echo ==== Creando la imagen de la BBDD (5/10) ====
+cd "%initial_dir%\temp\2023-BIN-1-G1_TruckLogistic\backend\hr\hr-model\src\main\db"
+wsl.exe docker build -t 2023-bin-1-g1-db .
+echo ==== Creando la imagen del backend (6/10) ====
+cd "%initial_dir%\temp\2023-BIN-1-G1_TruckLogistic\backend\hr"
+wsl.exe docker build -t 2023-bin-1-g1-backend .
+echo ==== Creando la imagen del frontend (7/10) ====
+cd "%initial_dir%\temp\2023-BIN-1-G1_TruckLogistic\frontend"
+wsl.exe docker build -t 2023-bin-1-g1-frontend .
+echo ==== Eliminando repositorios (8/10) ====
+cd "%initial_dir%"
+rmdir /s /q "temp"
+echo ==== Lanzando docker compose (9/10) ====
+wsl.exe docker compose up -d
+echo ==== Visualizando logs de docker compose (10/10) ====
+wsl.exe docker compose logs -f
