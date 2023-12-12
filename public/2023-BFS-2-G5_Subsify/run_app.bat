1f8bbd63da513186d46dbf2cffb406141fae3868
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2023-12-12 11:53:33 +0100

    Add 2023-BFS-2-G5_Subsify demo

---
+++ public/2023-BFS-2-G5_Subsify/run_app.bat
@@ -0,0 +1,29 @@
+@echo off
+for %%F in ("%~dp0.") do set "initial_dir=%%~fF"
+echo ==== Detener app con docker compose (1/10) ====
+wsl.exe docker compose down
+echo ==== Crear carpeta temporal (2/10) ====
+mkdir "%initial_dir%\temp"
+cd "%initial_dir%\temp"
+echo ==== Clonar repositorio (3/10) ====
+git clone --single-branch --branch docker https://github.com/CampusDual/2023-BFS-2-G5_Subsify
+echo ==== Limpiando imagenes antiguas (4/10) ====
+wsl.exe docker image rm 2023-bfs-2-g5-db
+wsl.exe docker image rm 2023-bfs-2-g5-backend
+wsl.exe docker image rm 2023-bfs-2-g5-frontend
+echo ==== Creando la imagen de la BBDD (5/10) ====
+cd "%initial_dir%\temp\2023-BFS-2-G5_Subsify\backend\backend-model\src\main\db"
+wsl.exe docker build -t 2023-bfs-2-g5-db .
+echo ==== Creando la imagen del backend (6/10) ====
+cd "%initial_dir%\temp\2023-BFS-2-G5_Subsify\backend"
+wsl.exe docker build -t 2023-bfs-2-g5-backend .
+echo ==== Creando la imagen del frontend (7/10) ====
+cd "%initial_dir%\temp\2023-BFS-2-G5_Subsify\frontend"
+wsl.exe docker build -t 2023-bfs-2-g5-frontend .
+echo ==== Eliminando repositorios (8/10) ====
+cd "%initial_dir%"
+rmdir /s /q "temp"
+echo ==== Lanzando docker compose (9/10) ====
+wsl.exe docker compose up -d
+echo ==== Visualizando logs de docker compose (10/10) ====
+wsl.exe docker compose logs -f
