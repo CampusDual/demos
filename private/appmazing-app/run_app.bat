9759a2d5ea734a0ca6339c51b40221940451680c
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2023-12-12 15:57:22 +0100

    Modify appmazing-app scripts

--- private/appmazing-app/run_app.bat
+++ private/appmazing-app/run_app.bat
@@ -1,9 +1,10 @@
 @echo off
+for %%F in ("%~dp0.") do set "initial_dir=%%~fF"
 echo ==== Detener app con docker compose (1/10) ====
 wsl.exe docker compose down
 echo ==== Crear carpeta temporal (2/10) ====
-mkdir temp
-cd temp
+mkdir "%initial_dir%\temp"
+cd "%initial_dir%\temp"
 echo ==== Clonar repositorio para el backend (3/10) ====
 git clone https://github.com/CampusDual/appmazing-back.git
 echo ==== Clonar repositorio para el frontend (4/10) ====
@@ -12,15 +13,13 @@ echo ==== Limpiando imagenes antiguas (5/10) ====
 wsl.exe docker image rm appmazingback
 wsl.exe docker image rm appmazingfront
 echo ==== Creando la imagen de backend (6/10) ====
-cd appmazing-back
+cd "%initial_dir%\temp\appmazing-back"
 wsl.exe docker build -t appmazingback .
-cd ..
 echo ==== Creando la imagen de frontend (7/10) ====
-cd appmazing-front
+cd "%initial_dir%\temp\appmazing-front"
 wsl.exe docker build -t appmazingfront .
-cd ..
 echo ==== Eliminando repositorios (8/10) ====
-cd ..
+cd "%initial_dir%"
 rmdir /s /q "temp"
 echo ==== Lanzando docker compose (9/10) ====
 wsl.exe docker compose up -d
--- private/appmazing-app/run_app.sh
+++ private/appmazing-app/run_app.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+initial_dir=$(cd "$(dirname "$0")" && pwd)
 echo "==== Detener app con docker compose (1/10) ===="
 docker compose down
 echo "==== Crear carpeta temporal (2/10) ===="
-mkdir temp
-cd temp
+mkdir "$initial_dir/temp"
+cd "$initial_dir/temp"
 echo "==== Clonar repositorio para el backend (3/10) ===="
 git clone https://github.com/CampusDual/appmazing-back.git
 echo "==== Clonar repositorio para el frontend (4/10) ===="
@@ -12,16 +13,14 @@ echo "==== Limpiando imágenes antiguas (5/10) ===="
 docker image rm appmazingback
 docker image rm appmazingfront
 echo "==== Creando la imagen de backend (6/10) ===="
-cd appmazing-back
+cd "$initial_dir/temp/appmazing-back"
 docker build -t appmazingback .
-cd ..
 echo "==== Creando la imagen de frontend (7/10) ===="
-cd appmazing-front
+cd "$initial_dir/temp/appmazing-front"
 docker build -t appmazingfront .
-cd ..
 echo "==== Eliminando repositorios (8/10) ===="
-cd ..
-rm -rf temp
+cd "$initial_dir"
+rm -rf "temp"
 echo "==== Lanzando docker compose (9/10) ===="
 docker compose up -d
 echo "==== Visualizando logs de docker compose (10/10) ===="
