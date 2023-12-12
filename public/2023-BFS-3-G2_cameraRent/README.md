e7b92230f2ef10ffadea30ef80f20798bbef871f
Pablo Martínez Kirsten <pablo.martinez@imatia.com>
2023-12-12 15:43:39 +0100

    Add 2023-BFS-3-G2_cameraRent demo

---
+++ public/2023-BFS-3-G2_cameraRent/README.md
@@ -0,0 +1,46 @@
+# 2023-BFS-3-G2_cameraRent
+## Repositorio
+* [Repositorio](https://github.com/CampusDual/2023-BFS-3-G2_cameraRent)
+## Presentación
+* [Visualización en Github](https://github.com/CampusDual/2023-BFS-3-G2_cameraRent/blob/docker/demo_day/cameraRent_demo.pdf)
+* [Descargar PDF](https://raw.github.com/CampusDual/2023-BFS-3-G2_cameraRent/docker/demo_day/cameraRent_demo.pdf)
+## Vídeo Demo Day
+* NO DISPONIBLE TEMPORALMENTE [Enlace al vídeo]( )
+## Base de datos
+* Driver: `PostgreSQL`
+* Host: `localhost`
+* Puerto: `9064`
+* Base de datos: `2023_BFS_3_G2`
+* Usuario: `2023_BFS_3_G2`
+* Contraseña: `b2f34VKzTvt2`
+## Backend y Frontend
+* Backend URL: [http://localhost:30081](http://localhost:30081)
+* Frontend URL: [http://localhost:4244](http://localhost:4244)
+* Usuario: `david.b`
+* Contraseña: `daviduser`
+
+# Requisitos
+* Docker (con WSL)
+* Docker compose (v2)
+* Git
+
+# Windows
+## Iniciar aplicación
+```
+2023-BFS-3-G2_cameraRent> run_app.bat
+```
+## Parar la aplicación
+```
+2023-BFS-3-G2_cameraRent> wsl.exe docker compose down
+```
+
+# WSL
+## Iniciar aplicación
+```
+2023-BFS-3-G2_cameraRent> chmod +x run_app.sh
+2023-BFS-3-G2_cameraRent> ./run_app.sh
+```
+## Parar la aplicación
+```
+2023-BFS-3-G2_cameraRent> docker compose down
+```
---
+++ public/2023-BFS-3-G2_cameraRent/run_app.bat
@@ -0,0 +1,29 @@
+@echo off
+for %%F in ("%~dp0.") do set "initial_dir=%%~fF"
+echo ==== Detener app con docker compose (1/10) ====
+wsl.exe docker compose down
+echo ==== Crear carpeta temporal (2/10) ====
+mkdir "%initial_dir%\temp"
+cd "%initial_dir%\temp"
+echo ==== Clonar repositorio (3/10) ====
+git clone --single-branch --branch docker https://github.com/CampusDual/2023-BFS-3-G2_cameraRent
+echo ==== Limpiando imagenes antiguas (4/10) ====
+wsl.exe docker image rm 2023-bfs-3-g2-db
+wsl.exe docker image rm 2023-bfs-3-g2-backend
+wsl.exe docker image rm 2023-bfs-3-g2-frontend
+echo ==== Creando la imagen de la BBDD (5/10) ====
+cd "%initial_dir%\temp\2023-BFS-3-G2_cameraRent\cameraback\cameraback-model\src\main\db"
+wsl.exe docker build -t 2023-bfs-3-g2-db .
+echo ==== Creando la imagen del backend (6/10) ====
+cd "%initial_dir%\temp\2023-BFS-3-G2_cameraRent\cameraback"
+wsl.exe docker build -t 2023-bfs-3-g2-backend .
+echo ==== Creando la imagen del frontend (7/10) ====
+cd "%initial_dir%\temp\2023-BFS-3-G2_cameraRent\camerafront"
+wsl.exe docker build -t 2023-bfs-3-g2-frontend .
+echo ==== Eliminando repositorios (8/10) ====
+cd "%initial_dir%"
+rmdir /s /q "temp"
+echo ==== Lanzando docker compose (9/10) ====
+wsl.exe docker compose up -d
+echo ==== Visualizando logs de docker compose (10/10) ====
+wsl.exe docker compose logs -f
